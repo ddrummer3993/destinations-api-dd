1058629a25d567b05a5d8c603905e0c5a8531e33
ddrummer <drumkdav@amazon.com>
2022-07-22 11:13:44 -0400

    added backend .post functionality

--- helperFuncs.js
+++ helperFuncs.js
@@ -1,3 +1,8 @@
+import axios from 'axios';
+
+
+// GET helpers
+
 function filterObj({ objToFilter: obj, filterValue }) {
     const filteredObj = {};
   
@@ -23,4 +28,32 @@ export function filterDestinations({city, destinationsDB, res}) {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  // POST helpers
+
+  export async function getUnsplashPhoto({ destName, locName }) {
+    // Use destName and locName as query parameters to get a photo related to them from the Unsplash API
+    const URL = `https://api.unsplash.com/search/photos?client_id=zPyO6m0ezgkOS-Tc0Co64-y6MqTXCULFL-TcXfxBrLc&query=${destName} ${locName}`;
+  
+    // TODO Use the URL to fetch photos from Unsplash and log those photos
+    // fetch(URL)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     const allPhotos = data.results;
+    //     const randIdx = Math.floor(Math.random() * allPhotos.length);
+    //     const randPhoto = allPhotos[randIdx];
+    //     return randPhoto;
+    //   });
+  
+    const response = await axios.get(URL);
+  
+    const allPhotos = await response.data.results;
+    const randIdx = Math.floor(Math.random() * allPhotos.length);
+    const randPhoto = allPhotos[randIdx];
+  
+    if (randPhoto === undefined) {
+      return "https://images.pexels.com/photos/3155666/pexels-photo-3155666.jpeg?cs=srgb&dl=pexels-asad-photo-maldives-3155666.jpg&fm=jpg";
+    }
+    return randPhoto.urls.thumb;
   }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import cors from 'cors';
 import express from 'express';
-import { filterDestinations } from "./helperFuncs.js"
+import { filterDestinations, getUnsplashPhoto } from "./helperFuncs.js"
 
 const server = express(); // This server is deaf
 
+//middleware setup
 server.use(cors());
+server.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
@@ -18,30 +20,56 @@ const destinationsDB = {
     location: "Paris",
     photo:
       "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    description:'',
   },
   234567: {
     destination: "Louvre",
     location: "Paris",
     photo:
       "https://media.architecturaldigest.com/photos/5900cc370638dd3b70018b33/3:4/w_1547,h_2063,c_limit/Secrets%20of%20Louvre%201.jpg",
+    description:'',
   },
   345678: {
     destination: "Big Ben",
     location: "London",
     photo:
       "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+    description:'',
   },
 };
 
 // CREATE
 
+//POST /destinations
+//needs/receives {location, destination, description,}
+//BOTH location and destination are required - description optional.
+//create new destination inside destinationsDB with a photo from unsplash
 
+server.post('/destinations', async (req, res) => {
+  const newDest = req.body
+  const destName = req.body.destination;
+  const locName = req.body.location;
+  console.log(newDest, destName, locName);
+
+  if (destName !== undefined && locName !== undefined) {
+    const photoURL = await getUnsplashPhoto({ destName, locName });
+    console.log(photoURL);
+    newDest.photo = photoURL;
+
+    destinationsDB[456789] = newDest;
+    res.status(201).send("done");
+  } else {
+    console.log("you MUST send a destination and location!");
+    res.status(400).send("you MUST send a destination and location!");
+  }
+})
 
 // READ
 
 // GET /destinations
 server.get('/destinations', (req, res) => {
-  const city = req.query.city
+  const city = req.query.city      //represents an object containing a property for each query strain parameter aka /destinations?city=Paris
+  console.log(req.query);
 
   filterDestinations({ city, destinationsDB, res });
 })
